29652adf31d369bb321f93a66a1f4937351bcc7d
Ray Matsumoto <raymat@master.cl.vanderbilt.edu>
2021-01-19 12:39:05 -0500

    Add function to calculate cn vs time

--- summary/plot_relaxation.py
+++ summary/plot_relaxation.py
@@ -53,6 +53,33 @@ def get_auc(data, idx):
     
     return auc
 
+def get_cn(data, idx):
+    from scipy.signal import argrelextrema
+    import scipy
+    """ Get integral of g(r) of first peak for CN"""
+    r = data['r']
+    g = data['g'][idx]
+    
+    #min1, _ = find_local_minima(r, data['g'][idx], 0.24)
+    # I have also tried setting the bounds to be 2.6 angstroms and 3.2 nm for all frames
+    min1 = 0.24
+    min2, _ = find_local_minima(r, data['g'][idx], 0.34) # Changed from 3.6 to 3.4
+    
+    # When this occurs, min2 is usually too low
+    if min1 == min2:
+        min2 = 0.34
+
+    min1_idx = np.where(np.isclose(r, min1))[0][0]
+    min2_idx = np.where(np.isclose(r, min2))[0][0]
+
+    r_peak = r[min1_idx:min2_idx]
+    g_peak = g[min1_idx:min2_idx]
+
+    #auc = np.trapz(g_peak[g_peak>1] - 1, r_peak[g_peak>1])
+    auc = np.trapz(g_peak*(r_peak)**2, r_peak)
+    
+    return auc
+
 def _pairing_func(x, a, b, c, d, e, f):
     """exponential function for fitting AUC data"""
     #y = a * np.exp(-(b * x)**c) + d * np.exp(-(e * x)**f)
@@ -89,12 +116,16 @@ aimd = {
     't': np.loadtxt('../aimd/water_form/t.txt')[::10]*0.0005,
     'g': np.loadtxt('../aimd/water_form/vhf.txt')[::10],
     'name': 'AIMD',
+    'volume': 3.83, # nm
+    'nwaters': 128,
 }
 aimd_330 = {
     'r': np.loadtxt('../aimd/330k/water_form/r.txt'),
     't': np.loadtxt('../aimd/330k/water_form/t.txt')[::10]*0.0005,
     'g': np.loadtxt('../aimd/330k/water_form/vhf.txt')[::10],
     'name': 'optB88_330K',
+    'volume': 3.83, # nm
+    'nwaters': 128,
 }
 
 aimd_filtered = {
@@ -102,6 +133,8 @@ aimd_filtered = {
     't': aimd['t'],
     'g': savgol_filter(aimd['g'], window_length=7, polyorder=3),
     'name': 'optB88 (filtered)',
+    'volume': 3.83, # nm
+    'nwaters': 128,
 }
 
 aimd_filtered_330 = {
@@ -109,6 +142,8 @@ aimd_filtered_330 = {
     't': aimd_330['t'],
     'g': savgol_filter(aimd_330['g'], window_length=7, polyorder=3),
     'name': 'optB88 at 330K (filtered)',
+    'volume': 3.83, # nm
+    'nwaters': 128,
 }
 
 bk3 = {
@@ -116,6 +151,8 @@ bk3 = {
     't': np.loadtxt('../bk3/nvt/t.txt'),
     'g': np.loadtxt('../bk3/nvt/vhf.txt'),
     'name': 'BK3',
+    'volume': 30.31, # nm
+    'nwaters': 1000,
 }
 
 dftb = {
@@ -123,6 +160,8 @@ dftb = {
     't': np.loadtxt('../dftb/water_form/2ns/t.txt'),
     'g': np.loadtxt('../dftb/water_form/2ns/vhf.txt'),
     'name': 'DFTB_noD3/3obw',
+    'volume': 7.49, # nm
+    'nwaters': 250,
 }
 
 dftb_d3 = {
@@ -130,6 +169,8 @@ dftb_d3 = {
     't': np.loadtxt('../dftb/water_form/2ns/d3_t.txt'),
     'g': np.loadtxt('../dftb/water_form/2ns/d3_vhf.txt'),
     'name': 'DFTB_D3/3obw',
+    'volume': 7.49, # nm
+    'nwaters': 250,
 }
 
 spce = {
@@ -137,6 +178,8 @@ spce = {
     't': np.loadtxt('../../spce_vhf/size_2/1000/total/t.txt'),
     'g': np.loadtxt('../../spce_vhf/size_2/1000/total/vhf.txt'),
     'name': 'SPC/E',
+    'volume': 30.31, # nm
+    'nwaters': 1000,
 }
 
 reaxff = {
@@ -144,6 +187,8 @@ reaxff = {
     't': np.loadtxt('../reaxff/water_form/t.txt')*0.0005,
     'g': np.loadtxt('../reaxff/water_form/vhf.txt'),
     'name': 'CHON-2017_weak',
+    'volume': 15.38, # nm
+    'nwaters': 512,
 }
 
 tip3p = {
@@ -151,6 +196,8 @@ tip3p = {
     't': np.loadtxt('../../spce_vhf/tip3p/1000/total/t.txt'),
     'g': savgol_filter(np.loadtxt('../../spce_vhf/tip3p/1000/total/vhf.txt'), window_length=7, polyorder=3),
     'name': 'TIP3P',
+    'volume': 30.31, # nm
+    'nwaters': 1000,
 }
 
 tip3p_ew = {
@@ -158,6 +205,8 @@ tip3p_ew = {
     't': np.loadtxt('../../spce_vhf/tip3p_ew/1000/total/t.txt'),
     'g': np.loadtxt('../../spce_vhf/tip3p_ew/1000/total/vhf.txt'),
     'name': 'TIP3P_EW',
+    'volume': 30.31, # nm
+    'nwaters': 1000,
 }
 
 IXS = {
@@ -198,7 +247,8 @@ def plot_peak_locations(datas):
         # Loop through times
         for i in range(0, t.shape[0], 5):
             g = data['g'][i][r_low:r_high]
-            r_max, g_max = find_local_maxima(r_range, g, 0.3)
+            r_max, g_max = find_local_maxima(r_range, g, 0.28)
+            print(r_max)
 
             plt.scatter(data['t'][i], r_max, color=get_color(data["name"]), label=data["name"])
             
@@ -264,15 +314,9 @@ def first_peak_auc(datas):
             I[i] = get_auc(data, i)
         ls = '--'
 
-        if data['name'] == 'IXS':
-            ax.semilogy(t, I, marker='.',
-                label=data['name'],
-                color='k')
-        else: 
-            ax.semilogy(t, I, ls=ls,
-                lw=2,
-                label=data['name'],
-                color=get_color(data['name']))
+        ax.semilogy(t, I, ls=ls,
+            lw=2,
+            label=data['name'])
         
         # Get finite values
         I_idx = np.where(~np.isnan(I))
@@ -281,7 +325,7 @@ def first_peak_auc(datas):
 
         #t = t[:30]
         #I = I[:30]
-        upper_limit = np.where(t < 0.3)[0][-1]
+        upper_limit = np.where(t < 0.28)[0][-1]
         t = t[:upper_limit]
         I = I[:upper_limit]
         #if data["name"] == "IXS":
@@ -321,7 +365,7 @@ def first_peak_auc(datas):
     plt.savefig("figures/first_peak_auc.png")
     df.to_csv("tables/first_peak_fits.csv")
 
-def plot_first_peak_subplot(datas):
+def plot_first_peak_subplot(datas, si=False):
     fontsize = 18
     labelsize = 18
     fig, axes = plt.subplots(1, 2, figsize=(16, 6))
@@ -335,22 +379,33 @@ def plot_first_peak_subplot(datas):
     max_r = list() 
     for data in datas:    
         maxs = np.zeros(len(data['t']))
+        r = data['r']
+        r_low = np.where(r > 0.15)[0][0]
+        r_high = np.where(r < 0.32)[0][-1]
+        r_range = r[r_low:r_high]
         for i, frame in enumerate(data['g']):
             if data['t'][i] < 0.0:
                 maxs[i] = np.nan
                 continue
-            maxs[i] = find_local_maxima(data['r'], frame, r_guess=0.26)[1]
+            g_range = frame[r_low:r_high]
+            #maxs[i] = find_local_maxima(data['r'], frame, r_guess=0.26)[1]
+            maxs[i] = find_local_maxima(r_range, g_range, r_guess=0.28)[1]
             if data['name'] == 'SPC/E':
-               max_r.append(find_local_maxima(data['r'], frame, r_guess=0.26)[0])
+               #max_r.append(find_local_maxima(data['r'], frame, r_guess=0.26)[0])
+               max_r.append(find_local_maxima(r_range, g_range, r_guess=0.28)[0])
         if data['name'] == 'IXS':
             ax.semilogy(data['t'], maxs-1, '.', lw=2, label=data['name'], color=get_color(data['name']))
         else:
             ax.semilogy(data['t'], maxs-1, ls='--', lw=2, label=data['name'], color=get_color(data['name']))
-    ax.set_xlim((0.00, 0.6))
+    if si:
+        ax.set_xlim((0.00, 1.25))
+        ax.xaxis.set_major_locator(MultipleLocator(0.25))
+    else:
+        ax.set_xlim((0.00, 0.6))
+        ax.xaxis.set_major_locator(MultipleLocator(0.1))
     ax.set_ylim((3e-2, 2.5))
     ax.set_ylabel(r'$g_1(t)-1$', fontsize=fontsize)
     ax.set_xlabel(r'Time, $t$, $ps$', fontsize=fontsize)
-    ax.xaxis.set_major_locator(MultipleLocator(0.1))
     ax.vlines(x=0.1, ymin=2e-2, ymax=2.5, color='k', ls='--')
     ax.tick_params(axis='both', labelsize=labelsize)
     fig.legend(bbox_to_anchor=(0.45, 1.15), loc='upper center', prop={'size': fontsize}, ncol=4)
@@ -382,16 +437,93 @@ def plot_first_peak_subplot(datas):
                 label=data['name'],
                 color=get_color(data['name']))
 
-        ax.xaxis.set_major_locator(MultipleLocator(0.1))
-        ax.set_xlim((0.00, 0.6))
+        if si:
+            ax.xaxis.set_major_locator(MultipleLocator(0.25))
+            ax.set_xlim((0.00, 1.25))
+        else:
+            ax.xaxis.set_major_locator(MultipleLocator(0.1))
+            ax.set_xlim((0.00, 0.6))
         ax.set_ylim((5e-3, 1.0))
         ax.set_ylabel(r'A($t$)', fontsize=fontsize)
         ax.set_xlabel(r'Time, $t$, $ps$', fontsize=fontsize)
         ax.tick_params(axis='both', labelsize=labelsize)
+    if si:
+        fig.savefig('figures/first_subplot_si.png', dpi=500, bbox_inches='tight')
+        fig.savefig('figures/first_subplot_si.pdf', dpi=500, bbox_inches='tight')
+    else:
+        fig.savefig('figures/first_subplot.png', dpi=500, bbox_inches='tight')
+        fig.savefig('figures/first_subplot.pdf', dpi=500, bbox_inches='tight')
 
-    fig.savefig('figures/first_subplot.png', dpi=500, bbox_inches='tight')
-    fig.savefig('figures/first_subplot.pdf', dpi=500, bbox_inches='tight')
+def plot_first_fit():
+    """ Plot compressed exponential fit for each method"""
+    df = pd.read_csv("tables/first_peak_fits.csv") 
+    fig, ax = plt.subplots(figsize=(6, 6))
+    time = np.arange(0, 2.0, 0.05)
+    for idx in range(len(df)):
+        method = df.loc[idx]
+        name = method[0]
+        A = method["A"]
+        tau = method["tau"]
+        gamma = method["gamma"]
 
-first_peak_auc(datas)
+        a_t = A * np.exp(-(time/tau)**gamma)
+        ax.plot(time, a_t, label=name)
+
+        ax.set_yscale('log')
+
+    pre = 0.448 * np.exp(-(time/0.1235)**1.57)
+    ax.plot(time, pre, label="IXS, 2018 Phys. Rev.")
+
+    ax.set_ylim(5e-3,1)
+    ax.set_xlim(0,1)
+    ax.set_xlabel('t/ps')
+    plt.legend()
+    fig.savefig('figures/tau_fit.pdf', dpi=500, bbox_inches='tight')
+
+def first_cn(datas):
+    """
+    Plot the coordination number of the first peak as a function of time
+    
+    parameters
+    ----------
+    datas : list
+        list of dictionaries that contain VHF data
+        
+    returns
+    -------
+    """
+    fig, ax = plt.subplots(figsize=(6, 6))
+    fontsize = 18
+    labelsize = 18
+    for data in datas:
+        if data["name"] == "IXS":
+            continue
+        r = data['r']
+        t = data['t']
+        g = data['g']
+        rho = 4 * np.pi * data['nwaters']/data['volume'] # molecules / nm^3
+
+        I = np.empty_like(t)
+        I[:] = np.nan
+        
+        # Get area under the curve
+        #for i in range(0, t.shape[0], 2):
+        for i in range(0, t.shape[0]):
+            I[i] = get_cn(data, i) * rho
+        ls = '--'
+
+        ax.plot(t[::2], I[::2], ls=ls,
+            lw=2,
+            label=data['name'])
+
+    ax.set_xlabel(r'Time, $t$, $ps$', fontsize=fontsize)
+    ax.set_ylabel('Coordination Number', fontsize=fontsize)
+    ax.set_xlim((0, 0.6))
+    fig.savefig("figures/cn_vs_t.pdf", dpi=500, bbox_inches='tight')
+        
+#plot_first_fit()
+#first_peak_auc(datas)
 #plot_peak_locations(datas)
+#plot_first_peak_subplot(datas, si=True)
 #plot_first_peak_subplot(datas)
+first_cn(datas)
