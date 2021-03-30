717892944970c0dc0376f94b80fe97bb409a4dfe
Ray Matsumoto <raymat@master.cl.vanderbilt.edu>
2021-03-30 11:15:53 -0400

    Start reformatting into a proper package

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "vhf_water_analysis",
+    version = "0.0.0",
+    author = "Ray Matsumoto",
+    author_email = "ray.a.matsumoto@vanderbilt.edu",
+    description = ("Analysis of water models through the van hove function"),
+)
