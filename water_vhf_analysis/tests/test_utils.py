4e13e5e7b51a2d23ee8695e1361a9319cdb43d04
Ray Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2021-05-06 17:24:44 -0500

    Add to utils tests that loads in data

--- water_vhf_analysis/tests/test_utils.py
+++ water_vhf_analysis/tests/test_utils.py
@@ -4,14 +4,69 @@ from water_vhf_analysis.utils import utils
 
 
 class TestUtils(object):
-    @pytest.mark.parametrize("model", ("spce", "bk3", "tip3p_ew"))
+    @pytest.mark.parametrize("model", ("spce", "bk3", "tip3p_ew",
+    "reaxff"))
     def test_get_txt(self, model):
         file_path = model + "/nvt_total_data/"
         utils.get_txt_file(file_path, "t_random.txt")
 
-    @pytest.mark.parametrize("model", ("spce", "bk3", "tip3p_ew"))
+    @pytest.mark.parametrize("model", ("spce", "bk3", "tip3p_ew",
+    "reaxff"))
     def test_read_txt_file(self, model):
         file_path = model + "/nvt_total_data/"
         time = np.loadtxt(utils.get_txt_file(file_path, "t_random.txt"))
 
-        assert time[0] == 0
+        if model == "reaxff":
+            last = 1.98
+        else:
+            last = 1.99
+        assert np.isclose(time[0], 0)
+        assert np.isclose(time[-1], last)
+
+    @pytest.mark.parametrize("model", ("spce", "bk3", "tip3p_ew"))
+    def test_read_r_txt_file(self, model):
+        file_path = model + "/nvt_total_data/"
+        r = np.loadtxt(utils.get_txt_file(file_path, "r_random.txt"))
+
+        assert np.isclose(r[0], 0.0025)
+        assert np.isclose(r[-1], 0.9975)
+
+
+    @pytest.mark.parametrize("model", ("spce", "bk3", "tip3p_ew"))
+    def test_read_vhf_txt_file(self, model):
+        file_path = model + "/nvt_total_data/"
+        vhf = np.loadtxt(utils.get_txt_file(file_path, "vhf_random.txt"))
+
+        assert np.isclose(vhf[0][-1], 1.0, rtol=1e-1)
+
+    @pytest.mark.parametrize("temp", ("300k", "330k"))
+    def test_read_aimd_txt_file(self, temp):
+        if temp == "300k":
+            file_path = "aimd/nvt_total_data/"
+        else:
+            file_path = "aimd/{}/nvt_total_data/".format(temp)
+        time = np.loadtxt(utils.get_txt_file(file_path, "t_random.txt"))
+
+        assert np.isclose(time[0], 0)
+        assert np.isclose(time[-1], 1.99)
+
+        r = np.loadtxt(utils.get_txt_file(file_path, "r_random.txt"))
+
+        assert np.isclose(r[0], 0.0025)
+        assert np.isclose(r[-1], 0.9975)
+
+    def test_read_dftb_txt_file(self):
+        file_path = "dftb/nvt_total_data/2ns"
+        time = np.loadtxt(utils.get_txt_file(file_path, "t_random.txt"))
+
+        assert np.isclose(time[0], 0)
+        assert np.isclose(time[-1], 1.99)
+
+        r = np.loadtxt(utils.get_txt_file(file_path, "r_random.txt"))
+
+        assert np.isclose(r[0], 0.0025)
+        assert np.isclose(r[-1], 0.9975)
+
+        vhf = np.loadtxt(utils.get_txt_file(file_path, "vhf_random.txt"))
+
+        assert np.isclose(vhf[0][-1], 1.0, rtol=1e-1)
