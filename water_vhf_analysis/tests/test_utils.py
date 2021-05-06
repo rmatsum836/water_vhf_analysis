f8b9cf698737f9ebd2242efe1cf310b7be4340c1
Ray Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2021-05-06 17:36:27 -0500

    Add utils tests for partials

--- water_vhf_analysis/tests/test_utils.py
+++ water_vhf_analysis/tests/test_utils.py
@@ -70,3 +70,44 @@ class TestUtils(object):
         vhf = np.loadtxt(utils.get_txt_file(file_path, "vhf_random.txt"))
 
         assert np.isclose(vhf[0][-1], 1.0, rtol=1e-1)
+
+    @pytest.mark.parametrize("model", ("spce", "bk3", "tip3p_ew",
+    "reaxff", "dftb"))
+    def test_read_partial_txt_file(self, model):
+        file_path = model + "/nvt_partial_data/"
+        if model == "reaxff":
+            last = 1.98
+        else:
+            last = 1.99
+
+        for pair in ["O_O", "O_H", "H_H"]:
+            time = np.loadtxt(utils.get_txt_file(file_path,
+                "t_random_{}.txt".format(pair)))
+
+            assert np.isclose(time[0], 0)
+            assert np.isclose(time[-1], last)
+
+            r = np.loadtxt(utils.get_txt_file(file_path,
+                "r_random_{}.txt".format(pair)))
+
+            assert np.isclose(r[0], 0.002)
+            assert np.isclose(r[-1], 0.798)
+
+    @pytest.mark.parametrize("temp", ("300k", "330k"))
+    def test_read_aimd_partial_txt_file(self, temp):
+        if temp == "300k":
+            file_path = "aimd/nvt_partial_data/"
+        else:
+            file_path = "aimd/{}/nvt_partial_data/".format(temp)
+        for pair in ["O_O", "O_H", "H_H"]:
+            time = np.loadtxt(utils.get_txt_file(file_path,
+                "t_random_{}.txt".format(pair)))
+
+            assert np.isclose(time[0], 0)
+            assert np.isclose(time[-1], 1.99)
+
+            r = np.loadtxt(utils.get_txt_file(file_path,
+                "r_random_{}.txt".format(pair)))
+
+            assert np.isclose(r[0], 0.002)
+            assert np.isclose(r[-1], 0.798)
