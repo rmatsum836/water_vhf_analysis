b1a5975459f3f64c3b50d9dac1b0a4ded68eb7bf
Ray Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2021-05-18 17:49:07 -0500

    Add tests for total peaks

--- water_vhf_analysis/tests/test_peaks.py
+++ water_vhf_analysis/tests/test_peaks.py
@@ -9,8 +9,52 @@ from water_vhf_analysis.utils import analysis, utils
 from scattering.utils.features import find_local_maxima, find_local_minima
 from water_vhf_analysis.utils.utils import get_txt_file
 
-class TestAnalysis(BaseTest):
+class TestPeaks(BaseTest):
     """Test to ensure peak heights are accurate"""
+
+    @pytest.mark.parametrize("model", ("spce", "tip3p_ew", "aimd", "bk3", "dftb"))
+    def test_total_first_peak_max(self, model):
+        model_dict = {
+            "name": model,
+            "r": np.loadtxt(get_txt_file(f"{model}/nvt_total_data", "r_random.txt")),
+            "t": np.loadtxt(get_txt_file(f"{model}/nvt_total_data", "t_random.txt")),
+            "g": np.loadtxt(get_txt_file(f"{model}/nvt_total_data", "vhf_random.txt")),
+        }
+
+        r = model_dict["r"]
+        r_low = np.where(r > 0.15)[0][0]
+        r_high = np.where(r < 0.32)[0][-1]
+        r_range = r[r_low:r_high]
+
+        maxs = list()
+        # Only look at first 80 frames
+        for i, frame in enumerate(model_dict["g"][:80]):
+            g_range = frame[r_low:r_high]
+            max_r, max_g = find_local_maxima(r_range, g_range, r_guess=0.28)
+            maxs.append(max_r)
+
+        maxs = np.array(maxs)
+
+        assert np.all((maxs > 0.23) & (maxs < 0.32))
+
+    @pytest.mark.parametrize("model", ("spce", "tip3p_ew", "aimd", "bk3", "dftb"))
+    def test_total_second_peak_max(self, model):
+        model_dict = {
+            "name": model,
+            "r": np.loadtxt(get_txt_file(f"{model}/nvt_total_data", "r_random.txt")),
+            "t": np.loadtxt(get_txt_file(f"{model}/nvt_total_data", "t_random.txt")),
+            "g": np.loadtxt(get_txt_file(f"{model}/nvt_total_data", "vhf_random.txt")),
+        }
+
+        maxs = list()
+        # Only look at first 80 frames
+        for i, frame in enumerate(model_dict["g"][:80]):
+            max_r, max_g = find_local_maxima(model_dict["r"], frame, r_guess=0.44)
+            maxs.append(max_r)
+
+        maxs = np.array(maxs)
+
+        assert np.all((maxs > 0.39) & (maxs < 0.5))
 
     @pytest.mark.parametrize("model", ("spce", "tip3p_ew", "aimd", "bk3", "dftb"))
     def test_hbond_peak_max(self, model):
